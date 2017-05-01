66ece1ce5a40197f3ba474b02cc6822cb2e85913
sewemark <sewerynjakub.markowicz@gmail.com>
2017-05-01 18:50:37 +0200

    TcpServer added

---
+++ Infrastructure/TcpServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DropBoxLoadBalancer.Infrastructure
+{
+    public class TcpServer
+    {
+        private TcpListener tcpListenre;
+        public TcpServer(int port)
+        {
+            tcpListenre = new TcpListener(IPAddress.Any, port);
+            tcpListenre.Start();
+        }
+        
+    }
+}
